00dbc040e9e324e4cc8e922cbdaecfaee3f3dc02
Takudzwa Gozo <thetakudzwagozo@gmail.com>
2022-04-25 11:49:25 +0200

    initial commit

---
+++ main.py
@@ -0,0 +1,27 @@
+import pandas as pd
+import numpy as np
+import sklearn
+from sklearn import linear_model
+
+
+data = pd.read_csv("student-mat.csv", sep=";")
+
+data = data[["G1", "G2", "G3", "studytime", "failures", "absences"]]
+
+predict = "G3"
+
+X = np.array(data.drop(columns=[predict]))
+y = np.array(data[predict])
+
+x_train, x_test, y_train, y_test = sklearn.model_selection.train_test_split(X, y, test_size=0.1)
+
+linear = linear_model.LinearRegression()
+
+linear.fit(x_train, y_train)
+acc = linear.score(x_test, y_test)
+print(acc)
+
+predictions = linear.predict(x_test)
+
+for x in range(len(predictions)):
+    print(predictions[x], x_test[x], y_test[x])
